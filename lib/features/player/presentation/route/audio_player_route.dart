6bc634dde1aa1d4edd7d674538057156070d2145
Prince <mnisiddique30@gmail.com>
2026-01-13 19:43:26 +0600

    wip: adds background of the app

--- lib/features/player/presentation/route/audio_player_route.dart
+++ lib/features/player/presentation/route/audio_player_route.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:playly/core/app/navigation/named_route.dart';
 import 'package:playly/core/presentation/cubit/now_playing_audio/now_playing_audio_cubit.dart';
+import 'package:playly/core/presentation/model/audio_model.dart';
 import 'package:playly/res/index.dart';
 
 class AudioPlayerRoute extends GoRoute {
@@ -19,27 +20,56 @@ class AudioPlayerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: AudioPlayerScreenContent());
+    return Scaffold(
+      body: BlocBuilder<NowPlayingAudioCubit, NowPlayingAudioState>(
+        builder: (context, state) {
+          return state.maybeWhen(
+            orElse: () => const SizedBox.shrink(),
+            nowPlaying: (audio) {
+              return AudioPlayerScreenContent(song: audio);
+            },
+          );
+        },
+      ),
+    );
   }
 }
 
 class AudioPlayerScreenContent extends StatelessWidget {
-  const AudioPlayerScreenContent({super.key});
+  const AudioPlayerScreenContent({super.key, required this.song});
+  final AudioModel song;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NowPlayingAudioCubit, NowPlayingAudioState>(
-      builder: (context, state) {
-        final title = state.maybeWhen(
-          orElse: () => vskEmpty,
-          nowPlaying: (audio) => audio.audio.title,
-        );
-        final id = state.maybeWhen(
-          orElse: () => -1,
-          nowPlaying: (audio) => audio.position,
-        );
-        return Center(child: Text("$title\n$id"));
-      },
+    return AppBackground(
+      child: Center(
+        child: Text(
+          song.audio.title,
+          style: Theme.of(
+            context,
+          ).textTheme.headlineMedium?.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class AppBackground extends StatelessWidget {
+  final Widget child;
+  const AppBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment(nk00, nkNegative0pt1),
+          radius: nk1pt2,
+          colors: [ColorGen.kCelestialGlow, ColorGen.kCelestialDeep],
+          stops: [0.0, 1.0],
+        ),
+      ),
+      child: child,
     );
   }
 }
