1baab35e2dd1e3793a635270038e9e8cc697a2f6
Prince <mnisiddique30@gmail.com>
2026-01-29 00:17:24 +0600

    wip: remove obsolute cubits

--- lib/core/presentation/provider/global_provider.dart
+++ lib/core/presentation/provider/global_provider.dart
@@ -1,13 +1,7 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:playly/core/app/injector/auto_injector.dart';
-import 'package:playly/core/presentation/cubit/now_playing_audio/now_playing_audio_cubit.dart';
 import 'package:playly/core/presentation/cubit/songs/songs_cubit.dart';
-import 'package:playly/features/player/presentation/cubit/playback/plaback_cubit.dart';
 
 final List<BlocProvider> globalProviders = [
   BlocProvider<SongsCubit>(create: (ctx) => getIt<SongsCubit>()),
-  BlocProvider<NowPlayingAudioCubit>(create: (ctx) => getIt<NowPlayingAudioCubit>()),
-  BlocProvider<PlayBackCubit>(create: (ctx) => getIt<PlayBackCubit>()),
 ];
